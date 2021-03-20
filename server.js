5d9031fb7c51c20ee48d5a3f3bfe98b6dd37ca9c
moussare <45038815+moussare@users.noreply.github.com>
2021-03-20 19:18:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -140,6 +140,7 @@ function getAllSubs(){
 }
 
 function log(json){
+  var today = new Date(); 
   let entete = today.getDate()+"/"+(today.getMonth()+1)+"/"+today.getFullYear()+" à "+today.getHours()+"/"+today.getMinutes()+"/"+today.getSeconds()
   logger.info('message', { date: entete, data:json })
 }
