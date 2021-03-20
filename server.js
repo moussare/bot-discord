547912eca920b8b8a9417c8a8cf7de12f668c495
moussare <45038815+moussare@users.noreply.github.com>
2021-03-20 09:31:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const  bodyParser = require('body-parser');
 const server = http.createServer(app);
 const axios = require('axios');
 const logger = require('heroku-logger');
-let json = {data:"dddd"}
-logger.info('message', { date:json})
+
 const verify = (req, res, buf, encoding) => {
   const expected = req.headers['x-hub-signature'];
   const calculated = 'sha256=' + crypto.createHmac('sha256', "secret").update(buf).digest('hex');
